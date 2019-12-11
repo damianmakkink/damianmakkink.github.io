7277ada7f194645c9b36ef46ecc8cdac14e60200
damianmakkink <vonoxnl@gmail.com>
2019-12-11 17:15:11 +0100

    Revert "Lightsaber Shop"
    This reverts commit 7b94a46ffb9d17e599d37efcbcac40f553757fac.

--- lightsabershop/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$db = mysqli_connect('s193.webhostingserver.nl:3306', 'deb81080_saberdbu', 'gAjlk5R2', 'deb81080_saberdb');
-	if (!$db) {
-		echo "Error: Unable to connect to MySQL." . PHP_EOL;
-		echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-		echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-		exit;
-	}
-?>
