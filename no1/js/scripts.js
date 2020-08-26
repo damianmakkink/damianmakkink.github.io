f262f863d64b99dfe311146eb2ac536d14b0d8fe
Damian Makkink <damian.makkink@pon.com>
2020-08-26 15:02:10 +0200

    Remove snapping

--- no1/js/scripts.js
+++ no1/js/scripts.js
@@ -31,13 +31,7 @@ function video1() {
             start: "top top",
             end: "+=5000",
             scrub: 1,
-            pin: true,
-            snap: {
-                snapTo: "labels", // snap to the closest label in the timeline
-                duration: {min: 0.2, max: 3}, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
-                delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
-                ease: "power1.inOut" // the ease of the snap animation ("power3" by default)
-            }
+            pin: true
         },
         onUpdate: render // use animation onUpdate instead of scrollTrigger's onUpdate
     });
@@ -83,13 +77,7 @@ function video2() {
             start: "top top",
             end: "+=5000",
             scrub: 1,
-            pin: true,
-            snap: {
-                snapTo: "labels", // snap to the closest label in the timeline
-                duration: {min: 0.2, max: 3}, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
-                delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
-                ease: "power1.inOut" // the ease of the snap animation ("power3" by default)
-            }
+            pin: true
         },
         onUpdate: render // use animation onUpdate instead of scrollTrigger's onUpdate
     });
